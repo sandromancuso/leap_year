46412ee0cedefacce1b5c34f783027aeccee872e
Sandro Mancuso <sandromancuso@yahoo.com>
2014-04-09 23:50:12 +0100

    First test: Not leap year if not divisible by 4

--- src/main/java/com/codurance/year/Year.java
+++ src/main/java/com/codurance/year/Year.java
@@ -1,4 +1,12 @@
 package com.codurance.year;
 
 public class Year {
+	public Year(int year) {
+
+
+	}
+
+	public Boolean leapYear() {
+		return false;
+	}
 }
--- src/test/java/com/codurance/year/YearShould.java
+++ src/test/java/com/codurance/year/YearShould.java
@@ -1,4 +1,16 @@
 package com.codurance.year;
 
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class YearShould {
+
+	@Test public void
+	not_be_a_leap_year_if_not_divisible_by_4() {
+		Year year = new Year(1997);
+	    assertThat(year.leapYear(), is(false));
+	}
+
 }
